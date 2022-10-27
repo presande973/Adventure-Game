f4b4dac5235d467b215c5b304b73f6c2cb8c2879
presande973 <presande973@SDS-L-F81HC63>
2022-10-27 11:18:25 -0400

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.optionLabel1 = new System.Windows.Forms.Label();
+            this.optionLabel2 = new System.Windows.Forms.Label();
+            this.outputLabel2 = new System.Windows.Forms.Label();
+            this.outputLabel3 = new System.Windows.Forms.Label();
+            this.outputLabl2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel1
+            // 
+            this.outputLabel1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabel1.Location = new System.Drawing.Point(9, 9);
+            this.outputLabel1.Name = "outputLabel1";
+            this.outputLabel1.Size = new System.Drawing.Size(514, 105);
+            this.outputLabel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Adventure_Game.Properties.Resources.Basement1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(537, 180);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // optionLabel1
+            // 
+            this.optionLabel1.AutoSize = true;
+            this.optionLabel1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.optionLabel1.Location = new System.Drawing.Point(58, 363);
+            this.optionLabel1.Name = "optionLabel1";
+            this.optionLabel1.Size = new System.Drawing.Size(18, 18);
+            this.optionLabel1.TabIndex = 2;
+            this.optionLabel1.Text = "/";
+            // 
+            // optionLabel2
+            // 
+            this.optionLabel2.AutoSize = true;
+            this.optionLabel2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.optionLabel2.Location = new System.Drawing.Point(58, 408);
+            this.optionLabel2.Name = "optionLabel2";
+            this.optionLabel2.Size = new System.Drawing.Size(18, 18);
+            this.optionLabel2.TabIndex = 3;
+            this.optionLabel2.Text = "/";
+            // 
+            // outputLabel2
+            // 
+            this.outputLabel2.AutoSize = true;
+            this.outputLabel2.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabel2.Location = new System.Drawing.Point(99, 362);
+            this.outputLabel2.Name = "outputLabel2";
+            this.outputLabel2.Size = new System.Drawing.Size(0, 23);
+            this.outputLabel2.TabIndex = 4;
+            this.outputLabel2.Click += new System.EventHandler(this.outputLabel2_Click);
+            // 
+            // outputLabel3
+            // 
+            this.outputLabel3.AutoSize = true;
+            this.outputLabel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.outputLabel3.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabel3.Location = new System.Drawing.Point(99, 404);
+            this.outputLabel3.Name = "outputLabel3";
+            this.outputLabel3.Size = new System.Drawing.Size(0, 23);
+            this.outputLabel3.TabIndex = 5;
+            // 
+            // outputLabl2
+            // 
+            this.outputLabl2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabl2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabl2.Location = new System.Drawing.Point(12, 312);
+            this.outputLabl2.Name = "outputLabl2";
+            this.outputLabl2.Size = new System.Drawing.Size(511, 50);
+            this.outputLabl2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(12, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(535, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.outputLabl2);
+            this.Controls.Add(this.outputLabel3);
+            this.Controls.Add(this.outputLabel2);
+            this.Controls.Add(this.optionLabel2);
+            this.Controls.Add(this.optionLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputLabel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label optionLabel1;
+        private System.Windows.Forms.Label optionLabel2;
+        private System.Windows.Forms.Label outputLabel2;
+        private System.Windows.Forms.Label outputLabel3;
+        private System.Windows.Forms.Label outputLabl2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel1.Text = "You wake up wondering were you are. With your vision still blurry you hear the sound of dripping water all around you. ";
+            outputLabl2.Text = "You feel the need to feel around. Do you?";
+            optionLabel1.Text = "Feel around";
+            optionLabel2.Text = "Not right now";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 16;
+            }
+            else if (page == 3)
+            {
+                page = 5;
+            }
+            else if (page == 4)
+            {
+                page = 16;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 16;
+            }
+            else if (page == 8)
+            {
+                page = 16;
+            }
+            else if (page == 9)
+            {
+                page = 16;
+            }
+            else if (page == 10)
+            {
+                page = 16;
+            }
+            else if (page == 11)
+            {
+                page = 13;
+            }
+            else if (page == 12)
+            {
+                page = 16;
+            }
+            else if (page == 13)
+            {
+                page = 14;
+            }
+            else if (page == 14)
+            {
+                page = 16;
+            }
+            else if (page == 15)
+            {
+                page = 16;
+            }
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel1.Text = "You wake up wondering were you are. With your vision still blurry you hear the sound of dripping water all around you.";
+                    outputLabl2.Text = "You feel the need to feel around. Do you?";
+                    optionLabel1.Text = "Feel around";
+                    optionLabel2.Text = "Not right now";
+                    break;
+                case 2:
+                    outputLabel1.Text = "As you feel around, the boards underneath you give out and you fall to your inky demise.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 3:
+                    outputLabel1.Text = "You decide to not feel around you. Your vision begins to come back and you realize the water you heard dripping wasn't water at all, its INK dripping from every crack on the walls around you layering the floor in a thick layer of ink.";
+                    outputLabl2.Text = "Do you try and stand?";
+                    optionLabel1.Text = "Stand";
+                    optionLabel2.Text = "Dont stand";
+                    break;
+                case 4:
+                    outputLabel1.Text = "As you stand the boards underneath you give out and you fall to your inky demise.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 5:
+                    outputLabel1.Text = "You decide not to stand and instead crawl through the ink towards a door that you see in the distance. Right before your about to reach the door an ink creature appears out of the ink behind you.";
+                    outputLabl2.Text = "Do you run?";
+                    optionLabel1.Text = "Run";
+                    optionLabel1.Text = "Dont run";
+                    break;
+                case 6:
+                    outputLabel1.Text = "You decide not to run and the creature reaches out and grabs your leg.";
+                    outputLabl2.Text = "Do you kick at the creature?";
+                    optionLabel1.Text = "Yes kick at the creature";
+                    optionLabel2.Text = "Dont kick at the creature";
+                    break;
+                case 7:
+                    outputLabel1.Text = "The creature pulls you into the depths of the ink.";
+                    outputLabl2.Text = "PLay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel1.Text = "No";
+                    break;
+                case 8:
+                    outputLabel1.Text = "You reach the door just in time and slam it shut. You then feel someone or something starring at you. ";
+                    outputLabl2.Text = "Do you turn around?";
+                    optionLabel1.Text = "Turn around";
+                    optionLabel2.Text = "Decide not to";
+                    break;
+                case 9:
+                    outputLabel1.Text = "You turn around and  get an axe lodged into your forehead.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 10:
+                    outputLabel1.Text = "After some time you get an axe lodged into the back of your head.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 11:
+                    outputLabel1.Text = "You turn  around and manage to duck in time as an axe slices the air above your head. You see that there is an open door to your left and a hallway to your right.";
+                    outputLabl2.Text = "What way do you go?";
+                    optionLabel1.Text = "Open door";
+                    optionLabel2.Text = "Hallway";
+                    break;
+                case 12:
+                    outputLabel1.Text = "You turn and run to the open door. You realize that whoever swung the axe is not chasing you for some reason, You run through the doorway and as you do the door slams shut behind you. You try and go back through as you realize you are back at the beginning.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 13:
+                    outputLabel1.Text = "You run down the hallway and make it to another door, you check to see that it is unlocked and lucky for you it is. You open the door and step outside to see your house just across the street. You walk to the crosswalk. ";
+                    outputLabl2.Text = "Do you push the button and wait?";
+                    optionLabel1.Text = "Push button";
+                    optionLabel2.Text = "Decide to J walk";
+                    break;
+                case 14:
+                    outputLabel1.Text = "You decide to J walk and make it home without a scratch. ";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 15:
+                    outputLabel1.Text = "You wait till the symbol appears before crossing. As you are crossing the road a Honda Civic runs a red light making you collateral damage to the vehicle.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 16:
+                    outputLabel1.Text = "Thank you for playing.";
+                    outputLabl2.Text = "";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    Refresh();
+                    Thread.Sleep(3000);
+                    Application.Exit();
+
+
+                    break;
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 1;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 8;
+            }
+            else if (page == 6)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                int chance = randGen.Next(1, 101);
+
+                if (chance <= 30)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    page = 11;
+                }
+
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 10)
+            {
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                page = 12;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 15)
+            {
+                page = 1;
+            }
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel1.Text = "You wake up wondering were you are. With your vision still blurry you hear the sound of dripping water all around you.";
+                    outputLabl2.Text = "You feel the need to feel around. Do you?";
+                    optionLabel1.Text = "Feel around";
+                    optionLabel2.Text = "Not right now";
+                    break;
+                case 2:
+                    outputLabel1.Text = "As you feel around, the boards underneath you give out and you fall to your inky demise.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 3:
+                    outputLabel1.Text = "You decide to not feel around you. Your vision begins to come back and you realize the water you heard dripping wasn't water at all, its INK dripping from every crack on the walls around you layering the floor in a thick layer of ink.";
+                    outputLabl2.Text = "Do you try and stand?";
+                    optionLabel1.Text = "Stand";
+                    optionLabel2.Text = "Dont stand";
+                    break;
+                case 4:
+                    outputLabel1.Text = "As you stand the boards underneath you give out and you fall to your inky demise.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 5:
+                    outputLabel1.Text = "You decide not to stand and instead crawl through the ink towards a door that you see in the distance. Right before your about to reach the door an ink creature appears out of the ink behind you.";
+                    outputLabl2.Text = "Do you run?";
+                    optionLabel1.Text = "Run";
+                    optionLabel1.Text = "Dont run";
+                    break;
+                case 6:
+                    outputLabel1.Text = "You decide not to run and the creature reaches out and grabs your leg.";
+                    outputLabl2.Text = "Do you kick at the creature?";
+                    optionLabel1.Text = "Yes kick at the creature";
+                    optionLabel2.Text = "Dont kick at the creature";
+                    break;
+                case 7:
+                    outputLabel1.Text = "The creature pulls you into the depths of the ink.";
+                    outputLabl2.Text = "PLay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel1.Text = "No";
+                    break;
+                case 8:
+                    outputLabel1.Text = "You reach the door just in time and slam it shut. You then feel someone or something starring at you. ";
+                    outputLabl2.Text = "Do you turn around?";
+                    optionLabel1.Text = "Turn around";
+                    optionLabel2.Text = "Decide not to";
+                    break;
+                case 9:
+                    outputLabel1.Text = "You turn around and  get an axe lodged into your forehead.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 10:
+                    outputLabel1.Text = "After some time you get an axe lodged into the back of your head.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 11:
+                    outputLabel1.Text = "You turn  around and manage to duck in time as an axe slices the air above your head. You see that there is an open door to your left and a hallway to your right.";
+                    outputLabl2.Text = "What way do you go?";
+                    optionLabel1.Text = "Open door";
+                    optionLabel2.Text = "Hallway";
+                    break;
+                case 12:
+                    outputLabel1.Text = "You turn and run to the open door. You realize that whoever swung the axe is not chasing you for some reason, You run through the doorway and as you do the door slams shut behind you. You try and go back through as you realize you are back at the beginning.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 13:
+                    outputLabel1.Text = "You run down the hallway and make it to another door, you check to see that it is unlocked and lucky for you it is. You open the door and step outside to see your house just across the street. You walk to the crosswalk. ";
+                    outputLabl2.Text = "Do you push the button and wait?";
+                    optionLabel1.Text = "Push button";
+                    optionLabel2.Text = "Decide to J walk";
+                    break;
+                case 14:
+                    outputLabel1.Text = "You decide to J walk and make it home without a scratch. ";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 15:
+                    outputLabel1.Text = "You wait till the symbol appears before crossing. As you are crossing the road a Honda Civic runs a red light making you collateral damage to the vehicle.";
+                    outputLabl2.Text = "Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 16:
+                    outputLabel1.Text = "Thank you for playing.";
+                    outputLabl2.Text = "";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    Refresh();
+                    Thread.Sleep(3000);
+                    Application.Exit();
+
+
+                    break;
+
+
+            }
+        }
+    }
+}
